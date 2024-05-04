a1140a0a75a4b7c53d8fbe892942dd39a8ad27c3
Hiro Cardenas <hiromar2015@gmail.com>
2024-05-04 09:09:52 -0500

    UPDATE: Controller CORS

--- src/main/java/com/example/discoverbackend/controllers/InmuebleController.java
+++ src/main/java/com/example/discoverbackend/controllers/InmuebleController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://localhost:4200")
+@CrossOrigin(origins = "https://discover-mock.netlify.app")
 @RestController
 @RequestMapping("/api")
 public class InmuebleController {
